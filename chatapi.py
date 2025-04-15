e1ea74bbc05e8cb4958814cb35176849f55b6408
HarshBTe <90679246+HarshBTe@users.noreply.github.com>
2025-04-15 22:28:26 +0530

    Update chatapi.py

--- chatapi.py
+++ chatapi.py
@@ -1,4 +1,5 @@
 import google.generativeai as ai
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -6,9 +7,13 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+api_key = os.getenv("GEMINI_API_KEY")
+
 myKey = "AIzaSyADf20vfZhtKPPjtexy4gpK3CqbSfpcuVM"
-ai.configure(api_key=myKey)
+ai.configure(api_key=api_key)
 model = ai.GenerativeModel("gemini-1.5-pro")
+
+
 
 @app.route('/chat', methods=['GET'])
 def chat():
