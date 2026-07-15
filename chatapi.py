81b6023d5ee56faf5b4c256deab69420f1d0796d
Harsh Dubey <90679246+HarshBTe@users.noreply.github.com>
2026-07-16 02:28:36 +0530

    Update chatapi.py

--- chatapi.py
+++ chatapi.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-api_key = os.getenv("AIzaSyADf20vfZhtKPPjtexy4gpK3CqbSfpcuVM")
+api_key = os.getenv("AQ.Ab8RN6Kc8NZdtGSWTFzVr-db1ePHhjLenOhkZkjl31hMz4RsdQ")
 
 
 
